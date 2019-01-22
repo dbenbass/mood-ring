1571c8dae5e725633809f5b419cdd10f9ad5fe43
dbenbass <dbenbass@gmail.com>
2019-01-22 15:20:46 -0500

    Added mood level seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+Mood.create(level: 1)
+Mood.create(level: 2)
+Mood.create(level: 3)
+Mood.create(level: 4)
+Mood.create(level: 5)
+Mood.create(level: 6)
+Mood.create(level: 7)
+Mood.create(level: 8)
+Mood.create(level: 9)
+Mood.create(level: 10)
